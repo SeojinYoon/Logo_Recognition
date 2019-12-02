bbd1ef4d6434e8275ce769f704fb8c56f6684189
김태현 <jtax90@naver.com>
2019-12-02 20:20:34 +0900

    네이버, 구글 카스 프레쉬, 사진 파일 및 코드

--- workspace/crawling/google_crawler.py
+++ workspace/crawling/google_crawler.py
@@ -5,10 +5,72 @@ Created on Mon Dec  2 17:39:32 2019
 @author: stu15
 """
 
-import time
-import urlib.request as req
+
+import time 
+import urllib.request as req
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selsnium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys
 
-url = "https://www.google.com/search?q=%EC%B9%B4%EC%8A%A4+%EB%A1%9C%EA%B3%A0&safe=active&rlz=1C1OKWM_koKR853KR854&sxsrf=ACYBGNQK3gMl9xZMAUszKAvJGiye2aeQZA:1575276034130&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj91L_DyJbmAhXSMd4KHbQaBvoQ_AUoAXoECAoQAw&biw=1920&bih=888"
+#GOOGLE
+url = "https://www.google.com/?gws_rd=ssl"
+driver = webdriver.Chrome("c:/data/chromedriVer.exe")
+driver.get(url)
+
+#검색어 창 선택
+e = driver.find_element_by_xpath("//*[@id='tsf']/div[2]/div[1]/div[1]/div/div[2]/input")
+
+#검색어 입력
+e.send_keys("카스 후레쉬")
+
+#엔터 입력
+e.submit()
+
+#이미지 항목 클릭
+driver.find_element_by_xpath('//*[@id="hdtb-msb-vis"]/div[2]/a').click()
+
+#이미지의 스크롤을 내림
+for i in range(1,50):
+    driver.find_element_by_tag_name('body').send_keys(Keys.END)
+    time.sleep(3)
+
+참고 :: 카스 후레쉬 이미지 자체가 얼마 없음
+
+#해당 사이트 정보 저장
+html = driver.page_source    
+soup = BeautifulSoup(html,'html.parser')
+
+
+#사이트 정보중 'img'의 'src' 주소만 추출
+params = []
+params2 = []
+
+for i in soup.select('img'):
+    if i.get('src') == None:
+        None
+    
+    else :
+        params.append(i['src'])
+        
+
+len(params)
+
+#/IMAGE로 시작하는 FILE이 있어, 사진으로 가져오지 못해서 params2에 가능한 이미지 주소만 따로 저장        
+for i in params:
+    if i.startswith("data"):
+        params2.append(i)
+    elif i.startswith("https"):
+        params2.append(i)
+        
+    if i.endswith("32x32.png"):
+        None
+
+len(params2)
+
+#사진 데이터 저장   
+x = 1
+for i in params2:
+    req.urlretrieve(i,"C:/data/TEAM_PROJECT/data/CASS FRESH/google_cass"+str(x)+".png")
+    x += 1 
+
+
---
+++ workspace/crawling/naver_crawler.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 19:48:50 2019
+
+@author: stu15
+"""
+
+import time 
+import urllib.request as req
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+#GOOGLE
+url = "https://search.naver.com/search.naver?sm=tab_sly.hst&where=image&query=&oquery=%EC%B9%B4%EC%8A%A4+%ED%94%84%EB%A0%88%EC%89%AC&tqi=UPgVWdp0YiRss4KGxfCsssssstR-161930"
+driver = webdriver.Chrome("c:/data/chromedriVer.exe")
+driver.get(url)
+
+#검색어 창 선택
+e = driver.find_element_by_id("nx_query")
+
+#검색어 입력
+e.send_keys("카스 프레쉬")
+
+#엔터 입력
+e.submit()
+
+
+#이미지의 스크롤을 내림
+for i in range(1,50):
+    driver.find_element_by_tag_name('body').send_keys(Keys.END)
+    time.sleep(5)
+    if i == 6:
+        driver.find_element_by_xpath("//*[@id='_sau_imageTab']/div[2]/div[8]/a").click()
+
+참고 :: 네이버는 6번째 루프에서 이미지 추가 버튼이 튀어나와서 스크롤이 안내려간다.
+그래서 6번째에 if문으로 이미지 추가 버튼 누르도록 처리
+
+#해당 사이트 정보 저장
+html = driver.page_source    
+soup = BeautifulSoup(html,'html.parser')
+
+
+#사이트 정보중 'img'의 'src' 주소만 추출
+params = []
+params2 = []
+
+for i in soup.select('img'):
+    if i.get('src') == None:
+        None
+    
+    else :
+        params.append(i['src'])
+        
+
+len(params)
+
+
+#사진 데이터 저장   
+x = 1
+for i in params:
+    req.urlretrieve(i,"C:/data/TEAM_PROJECT/data/CASS FRESH/naver_cass"+str(x)+".png")
+    x += 1 
+    
