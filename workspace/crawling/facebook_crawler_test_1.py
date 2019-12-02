cb3bbdb0eaec320a5c074e7271ccf605cfbb9910
1206woojin <1206woojin@gmail.com>
2019-12-02 17:20:13 +0900

    페이스북 크롤러 확인중(키워드 입력까지 됨)

---
+++ workspace/crawling/facebook_crawler_test_1.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 15:31:15 2019
+
+@author: stu21
+"""
+
+from bs4 import BeautifulSoup
+import urllib.request as req
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+url = 'https://www.facebook.com/'
+driver = webdriver.Chrome("c:/data/chromedriver.exe")
+driver.implicitly_wait(3)
+driver.get(url)    
+
+user = '-' # 아이디를 입력하세요
+mypass = '-' #비밀번호를 입력하세요
+
+#아이디 입력하는 곳
+inputid = driver.find_element_by_name("email")
+inputid.clear()
+inputid.send_keys(user)
+
+# 비밀번호 
+inputpw=driver.find_element_by_name("pass")
+inputpw.clear()
+inputpw.send_keys(mypass)
+
+# 로그인버튼
+loginbn=driver.find_element_by_name("login")
+loginbn.submit()
+
+# 키워드 입력
+inputpw=driver.find_element_by_name("q")
+inputpw.clear()
+inputpw.send_keys("맥주")
+
+# 엔터
+keyword=driver.find_element_by_class_name("_585_")
+keyword.submit()
+
+# 사진 카테고리로 가기
+photo=driver.find_element_by_class_name("")
+photo.submit()
