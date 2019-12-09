d91de432f88bf79ae9c4718013e6f71bdd1e6aaa
seojin <seojinyoon4@gmail.com>
2019-12-09 10:42:28 +0900

    이미지, 파일 처리를 위한 Utility 파일 생성

---
+++ workspace/Utility/fileUtil.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  8 15:09:50 2019
+
+@author: yoonseojin
+"""
+    
+def write(file_path, message):
+    with open(file_path, 'w') as file:
+        file.write(message)
+
+if __name__== "__main__":
+    # python 파일을 import 하기 위한 경로 설정(각자의 경로의 맞게 설정 필요))
+    import sys 
+    sys.path.append('/Users/yoonseojin/Logo_Recognition/workspace/utility')
+    
+    # import
+    import fileUtil
+    
+    # 에제
+    write('/Users/yoonseojin/Desktop/test.txt', '이런이런!')
+    
