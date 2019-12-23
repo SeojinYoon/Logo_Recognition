f791a69ca39d4f718386e3c63d3d67ab007f5832
seojin <seojinyoon4@gmail.com>
2019-12-23 11:17:18 +0900

    불필요 파일 제거

--- temp_hy.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec  3 11:20:36 2019
-
-@author: stu19
-"""
-
-dssfsdfsdf
--- workspace/crawling/TEST.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec  2 17:37:45 2019
-
-@author: stu15
-"""
-
-FDPF
